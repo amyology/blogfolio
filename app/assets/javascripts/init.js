bf64670ac69a83bbcf1f4c89005d7a387aa6eeae
Amy Chan <chamy09@gmail.com>
2016-10-22 14:28:48 -0500

    removed some stuff

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,18 +1,19 @@
 (function($){
+  
   $(function(){
-
     $('.button-collapse').sideNav({
       menuWidth: 210,
       closeOnClick: true
     });
 
     $('.parallax').parallax();
-
   });
 
   $(document).ready(function(){
     $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
+
     $('.scrollspy').scrollSpy();
+
     Materialize.updateTextFields();
   });
 
