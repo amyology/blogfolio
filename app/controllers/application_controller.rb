5c542371fcc4a6d9d977895f335085ebaca1f4b2
Amy Chan <chamy09@gmail.com>
2016-10-30 01:35:09 -0500

    added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+    def authenticate_admin!
+      unless admin_signed_in? && current_admin.superadmin
+        redirect_to '/'
+      end
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026225301) do
+ActiveRecord::Schema.define(version: 20161030060251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,9 @@ ActiveRecord::Schema.define(version: 20161026225301) do
     t.string   "email"
     t.string   "subject"
     t.text     "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "read",       default: false
   end
 
 end
