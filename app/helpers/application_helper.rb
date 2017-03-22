8d604f375be3703356f44e87ca52d53f7158e35f
Amy Chan <chamy09@gmail.com>
2017-03-22 13:33:53 -0500

    added dynamic titles and meta descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def meta_description(page_text)
+    content_for(:meta_description) { page_text }
+  end
+  
 end
